d101361de8b960a3c0b9c960f16af6192d610417
Jiawen Geng <technicalcute@gmail.com>
2021-03-19 15:18:26 +0000

    use new gitbook fork

---
+++ gitbook/test-e2e.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/gengjiawen/koajs-design-note.git
+cd koajs-design-note
+echo $PWD
+mkdir dist
+gitbook epub ./ dist/koa-design.epub
+gitbook mobi ./ dist/koa-design.mobi
+# gitbook pdf ./ dist/koa-design.pdf
